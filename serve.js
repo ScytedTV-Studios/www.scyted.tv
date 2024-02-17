0e177636b2db164dc2111e6495595a0d8023091a
Loyd Osborne <loyd.osborne@scyted.tv>
2024-02-17 15:59:15 -0700

    Commit

--- serve.js
+++ serve.js
@@ -4,45 +4,20 @@ const path = require('path');
 // Specify the Bundler executable path (if not in system PATH)
 const bundlerExecutable = 'bundler';
 
-// Specify the path to the Gemfile
-const gemfilePath = path.resolve(__dirname, 'docs', 'Gemfile');
+// Specify the Jekyll serve command with Bundler
+const jekyllServeCommand = `${bundlerExecutable} exec jekyll serve --trace --incremental`;
 
-// Specify the command to install dependencies
-const installCommand = `${bundlerExecutable} install --gemfile="${gemfilePath}"`;
+// Execute the Jekyll serve command
+const child = exec(jekyllServeCommand, { cwd: path.resolve(__dirname) });
 
-// Specify the command to run Jekyll serve
-const jekyllServeCommand = `${bundlerExecutable} exec jekyll serve`;
-
-// Execute the command to install dependencies
-const installChild = exec(installCommand, { cwd: path.resolve(__dirname, 'docs') });
-
-installChild.stdout.on('data', (data) => {
+child.stdout.on('data', (data) => {
   console.log(data);
 });
 
-installChild.stderr.on('data', (data) => {
+child.stderr.on('data', (data) => {
   console.error(data);
 });
 
-installChild.on('close', (code) => {
-  if (code === 0) {
-    console.log('Dependencies installed successfully.');
-
-    // Execute the Jekyll serve command if installation succeeds
-    const serveChild = exec(jekyllServeCommand, { cwd: path.resolve(__dirname, 'docs') });
-
-    serveChild.stdout.on('data', (data) => {
-      console.log(data);
-    });
-
-    serveChild.stderr.on('data', (data) => {
-      console.error(data);
-    });
-
-    serveChild.on('close', (serveCode) => {
-      console.log(`Jekyll process exited with code ${serveCode}`);
-    });
-  } else {
-    console.error('Failed to install dependencies.');
-  }
+child.on('close', (code) => {
+  console.log(`Jekyll process exited with code ${code}`);
 });
--- show/test.md
+++ show/test.md
@@ -7,4 +7,4 @@ description: Show Page Testing.
 <h3>Season 1</h3>
 <div class="video-grid" id="video-grid"></div>
 <div id="playlist-data" data-playlist-id="PL5irix3qFbXMJZbC_ANJA9FYuvKVTym8z"></div>
-<script src="../../src/show-scripts.js"></script>
+<script src="show-scripts.js"></script>
