048521b6444ba0307e5f674a4e01f33aff3ad953
Loyd Osborne <loyd.osborne@scyted.tv>
2024-08-26 18:20:31 -0600

    Commit

---
+++ activity/project-power-switch/v2/main.js
@@ -0,0 +1,68 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+// import rocketLogo from '/rocket.png';
+// import "./style.css";
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+
+  appendVoiceChannelName();
+  appendGuildAvatar();
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
+  // Read more about constructing a full URL and using external resources at
+  // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
+  const response = await fetch("/.proxy/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+  await discordSdk.commands.setActivity({
+    activity: { 
+      type: 2,
+      state: 'ScytedTV Activities Beta',
+      details: 'Project Power Switch',
+      assets: {
+        large_image: 'https://cdn.scyted.tv/assets/scytedtv/logos/scytedtv.jpg',
+        large_text: 'ScytedTV Studios',
+      }
+    }
+  });
+}
